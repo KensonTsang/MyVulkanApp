0bad2bd27a987cb990506e07d4a33268eeffa61b
KensonTsang <kenson.hwtsang@gmail.com>
2025-09-25 18:15:21 +0100

    init

---
+++ include/first_app.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "lve_window.hpp"
+
+namespace lve {
+
+    class FirstApp {    
+    public:
+
+        void run();
+    
+        static constexpr int WIDTH = 800;
+        static constexpr int HEIGHT = 600;
+
+    private:
+        LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+
+    };
+
+}
