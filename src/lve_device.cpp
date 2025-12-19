935f46d849c0e835cd46f78d787d39acb6ae66c3
KensonTsang <kenson.hwtsang@gmail.com>
2025-12-19 01:12:10 +0000

    Add VK_KHR_get_physical_device_properties_2 extension for improved device property queries

--- src/lve_device.cpp
+++ src/lve_device.cpp
@@ -319,6 +319,7 @@ namespace lve
 
     // Required for macOS MoltenVK portability
     extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    extensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
 
     return extensions;
   }
