dfef6d02a6963e8366dcbe4f6c95e6b42bc11bb6
KensonTsang <kenson.hwtsang@gmail.com>
2026-02-02 11:51:29 +0000

    Refactor LveModel to support indexed drawing; add Builder struct for model creation and implement index buffer management

--- include/lve_model.hpp
+++ include/lve_model.hpp
@@ -16,7 +16,12 @@ class LveModel {
     static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
   };
 
-  LveModel(LveDevice& lveDevice, const std::vector<Vertex>& vertices);
+  struct Builder {
+    std::vector<Vertex> vertices{};
+    std::vector<uint32_t> indices{};
+  };
+
+  LveModel(LveDevice& lveDevice, const LveModel::Builder& builder);
   ~LveModel();
 
   LveModel(const LveModel&) = delete;
@@ -27,10 +32,17 @@ class LveModel {
 
  private:
   void createVertexBuffers(const std::vector<Vertex>& vertices);
+  void createIndexBuffers(const std::vector<uint32_t>& indices);
 
   LveDevice& lveDevice;
+
   VkBuffer vertexBuffer;
   VkDeviceMemory vertexBufferMemory;
   uint32_t vertexCount;
+
+  bool hasIndexBuffer = false;
+  VkBuffer indexBuffer;
+  VkDeviceMemory indexBufferMemory;
+  uint32_t indexCount;
 };
 }  // namespace lve
